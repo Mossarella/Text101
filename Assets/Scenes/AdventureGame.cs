3418481e4805e172b0fefd64038235d62e1bca00
Pomelowy <mosno6@gmail.com>
2020-04-01 02:13:25 +0700

    create scriptableobject

---
+++ Assets/Scenes/AdventureGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    public Text textBox;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = ("test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/State/States.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="State")]
+public class States : ScriptableObject
+{
+    [TextArea(14, 10)] public string storyText;
+}
